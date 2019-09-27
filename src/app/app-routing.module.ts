931fcb987c6f78ea44123244b1181d29e36ea9dd
Rohit <rprajapati688@gmail.com>
2019-09-27 14:26:42 -0400

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AdminComponent } from './admin/admin.component';
+import { EmployeeComponent } from './employee/employee.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+ {path: '', component: HomeComponent},
+ {path: 'Admin', component: AdminComponent},
+ {path: 'Employee', component: EmployeeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
