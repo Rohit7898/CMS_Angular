edc5a2e4707f7a02270e6bbde2e062693209ef25
Rohit <rprajapati688@gmail.com>
2019-09-30 17:37:01 -0400

    order done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MexicanVendorComponent } from './mexican-vendor/mexican-vendor.componen
 import { FormsModule } from '@angular/forms';
 import { OrderComponent } from './order/order.component';
 import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [
     AppComponent,
--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {IndianVendorService } from '../indian-vendor/indian-vendor.service';
+import {AmericanVendorService } from '../american-vendor/american-vendor.service';
+import {MexicanVendorService } from '../mexican-vendor/mexican-vendor.service';
 
 @Component({
   selector: 'app-order',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderComponent implements OnInit {
 
-  constructor() { }
+  indianVendors: IndianVendor[];
+  errorMsg: any;
+  editIndianItem: IndianVendor;
+  americanVendors: AmericanVendor[];
+  editAmericanItem: AmericanVendor;
+  mexicanVendors: MexicanVendor[];
+  editMexicanItem: MexicanVendor;
+
+  constructor(public indianVendorService: IndianVendorService, public americanVendorService:AmericanVendorService, public mexicanVendorService:MexicanVendorService) { }
 
   ngOnInit() {
+    this.indianVendorService.getIndianItems().subscribe(
+      data => this.indianVendors = data,
+      error => this.errorMsg = error
+    );
+    this.americanVendorService.getAmericanItems().subscribe(
+      data => this.americanVendors = data,
+      error => this.errorMsg = error
+    );
+    this.mexicanVendorService.getMexicanItems().subscribe(
+      data => this.mexicanVendors = data,
+      error => this.errorMsg = error
+    );
+
   }
   public AmericanCollapsed = false;
   public IndianCollapsed = false;
