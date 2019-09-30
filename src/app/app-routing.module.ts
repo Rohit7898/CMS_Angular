77516e403c7d679cd6dd2a44002dc1db1c7c5abc
Rohit <rprajapati688@gmail.com>
2019-09-30 10:18:45 -0400

    Added VEndors

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,18 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AdminComponent } from './admin/admin.component';
 import { EmployeeComponent } from './employee/employee.component';
+import { IndianVendorComponent } from './indian-vendor/indian-vendor.component';
+import { AmericanVendorComponent } from './american-vendor/american-vendor.component';
+import { MexicanVendorComponent } from './mexican-vendor/mexican-vendor.component';
 
 
 const routes: Routes = [
  {path: '', component: HomeComponent},
  {path: 'Admin', component: AdminComponent},
- {path: 'Employee', component: EmployeeComponent}
+ {path: 'Employee', component: EmployeeComponent},
+ {path: 'Indian-Vendor', component: IndianVendorComponent},
+ {path: 'American-Vendor', component: AmericanVendorComponent},
+ {path: 'Mexican-Vendor', component: MexicanVendorComponent}
 ];
 
 @NgModule({
