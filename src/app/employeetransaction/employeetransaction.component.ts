5e09fb25f972977822ab9ec2b98d824a33463b6e
mufc1234 <PrasannaS@hexaware.com>
2019-10-01 21:20:43 -0400

    pls commit now

---
+++ src/app/employeetransaction/employeetransaction.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employeetransaction',
+  templateUrl: './employeetransaction.component.html',
+  styleUrls: ['./employeetransaction.component.css']
+})
+export class EmployeetransactionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
